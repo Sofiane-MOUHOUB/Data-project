f06f68f4278307aa76a77d24b8fd11c99e5fd442
Sofiane-MOUHOUB <sofiane.mouhoub@edu.esiee.fr>
2025-11-04 12:59:25 +0000

    Ajout du script init.py

---
+++ src/utils/__init__.py
@@ -0,0 +1,7 @@
+"""
+Module utilitaire pour le projet Dash sur les accidents à vélo.
+Contient les fonctions de chargement, nettoyage et traitement des données.
+"""
+
+from .clean_data import clean_data
+from .get_data import get_data
